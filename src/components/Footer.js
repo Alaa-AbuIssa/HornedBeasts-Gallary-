fe9ab5022f7b067e8dabde5049a56319d7742154
Alaa-AbuIssa <alaaabuissa95@gmail.com>
2021-05-16 23:29:00 +0300

    initial submitt

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Footer extends React.Component {
+    render() {
+        return (
+  <h4> Author: Alaa Abu Issa </h4>
+        )
+    }
+} 
+export default Footer;
