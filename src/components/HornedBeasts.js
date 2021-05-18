18baae632ea66516e59e04757a25c856ed375b3e
Alaa-AbuIssa <alaaabuissa95@gmail.com>
2021-05-18 08:30:41 +0300

    finished class2

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,16 +1,48 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+
 class HornedBeast extends React.Component {
-    render (){
+    constructor(props) {
+        super(props);
+        this.state =
+            { clickingCounter: 0, };
+
+
+    }
+    increaseFavorites = () => {
+        this.setState(
+            {
+                clickingCounter: this.state.clickingCounter +1,
+            }
+
+        );
+    }
+
+    render() {
+
 
         return (
-            <div>
-                <h2>{this.props.name}</h2>
-              
-               < img src={this.props.url}/>
 
-                <p>{this.props.description}</p>
+            <div>
+
+                <Card border="primary" style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title> {this.props.title}</Card.Title>
+                        
+                        <Card.Img onClick={this.increaseFavorites} src={this.props.img} />
+                        <Card.Text>
+                            {this.props.description}
+                        </Card.Text>
+                        <Card.Text>
+                        favourit💖: {this.state.clickingCounter}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <br />
+
             </div>
-        )
+        );
     }
 }
-export default HornedBeast ;
+export default HornedBeast;
